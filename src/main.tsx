0f4a221276fdee89f06fa28343d91003068e8b20
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 11:00:56 +0200

    Enable demo worker and supervisor switching

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { DemoProfileSwitcher, installDemoFetchIdentity } from './DemoProfileSwitcher';
 import './styles.css';
 import './responsive.css';
 import './layout-fixes.css';
 import './uploads.css';
+import './demo-profile.css';
+
+installDemoFetchIdentity();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><App /><DemoProfileSwitcher /></React.StrictMode>
 );
