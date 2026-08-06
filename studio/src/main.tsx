73c36bcc93e7723c0a1bc99dec2a119676cfc85c
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 16:22:37 +0200

    Switch Studio palette to atomic imports

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StudioWorkspace } from './StudioWorkspace';
+import { StudioWorkspaceAtomic } from './StudioWorkspaceAtomic';
 import './styles.css';
 import './palette.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><StudioWorkspace /></React.StrictMode>
+  <React.StrictMode><StudioWorkspaceAtomic /></React.StrictMode>
 );
