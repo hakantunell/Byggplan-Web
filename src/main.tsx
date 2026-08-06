9b2e405eedb16ae90cc37ba716aeb6c2fe28b797
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 10:23:19 +0200

    Load mobile layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './responsive.css';
+import './layout-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
