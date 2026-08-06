354664404ef8b129ae4c19325a51ae08f00e5490
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 18:37:33 +0200

    Enable classified component workspace

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StudioWorkspaceAtomic } from './StudioWorkspaceAtomic';
+import { StudioWorkspace } from './StudioWorkspace';
 import './styles.css';
 import './palette.css';
+import './classifications.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><StudioWorkspaceAtomic /></React.StrictMode>
+  <React.StrictMode><StudioWorkspace /></React.StrictMode>
 );
