92a619e948f2a79c83f9186bd9deb871ee6dc653
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 21:13:11 +0200

    Use single Studio shell for project and control plan views

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StudioWorkspace } from './StudioWorkspace';
-import { ControlPlanOverlay } from './ControlPlanOverlay';
-import { ControlPlanRailBridge } from './ControlPlanRailBridge';
-import { ControlPlanInlineMode } from './ControlPlanInlineMode';
+import { StudioShell } from './StudioShell';
 import './styles.css';
 import './palette.css';
 import './classifications.css';
 import './control-plan.css';
-import './control-plan-hotfix.css';
-import './control-plan-inline.css';
-import './control-plan-navigation-final.css';
+import './studio-shell.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StudioWorkspace />
-    <ControlPlanOverlay />
-    <ControlPlanRailBridge />
-    <ControlPlanInlineMode />
+    <StudioShell />
   </React.StrictMode>
 );
