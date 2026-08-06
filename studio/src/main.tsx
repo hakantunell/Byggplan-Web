781e420c6ce85ec2b7130eb7f41f62b9eaedab2c
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 20:32:23 +0200

    Use control plan as primary Studio view

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { StudioWorkspace } from './StudioWorkspace';
 import { ControlPlanOverlay } from './ControlPlanOverlay';
 import { ControlPlanRailBridge } from './ControlPlanRailBridge';
+import { ControlPlanInlineMode } from './ControlPlanInlineMode';
 import './styles.css';
 import './palette.css';
 import './classifications.css';
 import './control-plan.css';
 import './control-plan-hotfix.css';
+import './control-plan-inline.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StudioWorkspace />
     <ControlPlanOverlay />
     <ControlPlanRailBridge />
+    <ControlPlanInlineMode />
   </React.StrictMode>
 );
