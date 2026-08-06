e948775412681d6d6fe31a5a9ea0dfa149317034
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-06 19:39:04 +0200

    Expose graphical control plan view

--- studio/src/main.tsx
+++ studio/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StudioWorkspace } from './StudioWorkspace';
+import { ControlPlanOverlay } from './ControlPlanOverlay';
 import './styles.css';
 import './palette.css';
 import './classifications.css';
+import './control-plan.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><StudioWorkspace /></React.StrictMode>
+  <React.StrictMode>
+    <StudioWorkspace />
+    <ControlPlanOverlay />
+  </React.StrictMode>
 );
