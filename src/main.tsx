cacf151bd8d8fe89bc2296f57938eb49b35fc044
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-12 18:20:06 +0200

    Enable governing document badges in mobile app

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppErrorBoundary } from './AppErrorBoundary';
 import { DemoProfileSwitcher, installDemoFetchIdentity } from './DemoProfileSwitcher';
 import { installProjectSupportBridge } from './project-support-bridge';
 import { installMobileActivityScope } from './mobile-activity-scope';
+import { installGoverningBadges } from './governing-badges';
 import { ProjectDocumentsBar } from './ProjectDocumentsBar';
 import './styles.css';
 import './responsive.css';
@@ -15,10 +16,12 @@ import './supervisor.css';
 import './navigation.css';
 import './support-attachments.css';
 import './project-documents-bar.css';
+import './governing-badges.css';
 
 installDemoFetchIdentity();
 installProjectSupportBridge();
 installMobileActivityScope();
+installGoverningBadges();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
