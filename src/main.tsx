cb19e1a6b101a2b737df0c4be29398f008aea753
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-14 11:03:32 +0200

    Enable optional documentation on mobile activities

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppErrorBoundary } from './AppErrorBoundary';
 import { DemoProfileSwitcher, installDemoFetchIdentity } from './DemoProfileSwitcher';
 import { installProjectSupportBridge } from './project-support-bridge';
 import { installMobileActivityScope } from './mobile-activity-scope';
+import { installActivityOwnDocumentation } from './activity-own-documentation-bridge';
 import { ProjectDocumentsBar } from './ProjectDocumentsBar';
 import './styles.css';
 import './responsive.css';
@@ -19,6 +20,7 @@ import './project-documents-bar.css';
 installDemoFetchIdentity();
 installProjectSupportBridge();
 installMobileActivityScope();
+installActivityOwnDocumentation();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
