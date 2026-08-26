478d14f7b90143a6de4429d2ee4b18004484d894
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-26 11:42:52 +0200

    Remove global querySelector override

--- studio/src/build-doc-preview-dom.d.ts
+++
@@ -1,4 +0,0 @@
-// Narrow DOM typing used by BuildDocumentationReport. Runtime code still guards for a missing preview element.
-interface Document {
-  querySelector<E extends Element = Element>(selectors: '.buildDocPreview'): E;
-}
