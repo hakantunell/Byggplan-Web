77ef036c5ed624ece385e746ee5e241db5961483
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-09-17 15:31:38 +0200

    Fix System admin scrolling for module validation

--- studio/src/SystemAdministrationView.tsx
+++ studio/src/SystemAdministrationView.tsx
@@ -1,11 +1,12 @@
 import {SystemWorkspacesView} from './SystemWorkspacesView';
 import {MasterModuleValidation} from './MasterModuleValidation';
+import './system-administration.css';
 
 type Props={currentProjectId?:string;currentProjectName?:string};
 
 export function SystemAdministrationView(props:Props){
- return <>
+ return <div className="systemAdministrationView">
   <SystemWorkspacesView {...props}/>
-  <div className="systemWorkspacesView"><div className="systemWorkspaceGrid"><section/><aside><MasterModuleValidation/></aside></div></div>
- </>;
+  <div className="systemWorkspacesView systemAdministrationValidation"><div className="systemWorkspaceGrid"><section/><aside><MasterModuleValidation/></aside></div></div>
+ </div>;
 }
